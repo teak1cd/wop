c9de7110c70fe2d0ba2568f9eb0c5104c42dd4c4
teak1cd <isenne@sterneschool.org>
2017-01-02 18:42:04 -0800

    Create url.js

---
+++ js/url.js
@@ -0,0 +1,16 @@
+function URL(){
+  return this;
+}
+URL.prototype=Object.assign(URL.prototype,{
+  get(){
+  this = new URL();
+  var loc = window.location.split("?").shift();
+  var data = loc.split(",");
+  for(var i = 0;i<data.length;i++){
+    var variable = data[i].split(":");
+    eval("function _gd(){return "+variable[1]+"}");
+    this[variable[0]]=_gd();
+  }
+  return this;
+  }
+});
