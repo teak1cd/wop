2e2f8ee583d65f7b817eca337b8cdcf1ce9f26cf
teak1cd <isenne@sterneschool.org>
2017-01-02 18:44:00 -0800

    Update url.js

--- js/url.js
+++ js/url.js
@@ -1,16 +1,16 @@
-function URL(){
+function LE(){
   return this;
 }
-URL.prototype=Object.assign(URL.prototype,{
+LE.prototype=Object.assign(LE.prototype,{
   get(){
-  this = new URL();
-  var loc = window.location.split("?").shift();
-  var data = loc.split(",");
-  for(var i = 0;i<data.length;i++){
-    var variable = data[i].split(":");
-    eval("function _gd(){return "+variable[1]+"}");
-    this[variable[0]]=_gd();
-  }
-  return this;
+    this = new URL();
+    var loc = window.location.split("?").shift();
+    var data = loc.split(",");
+    for(var i = 0;i<data.length;i++){
+      var variable = data[i].split(":");
+      eval("function _gd(){return "+variable[1]+"}");
+      this[variable[0]]=_gd();
+    }
+    return this;
   }
 });
